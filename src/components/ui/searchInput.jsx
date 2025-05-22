d723229c207db9b7b8e5ca980ee5b2c1880d9397
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-22 20:08:25 +0600

    Search Item   🏩

--- src/components/ui/searchInput.jsx
+++ src/components/ui/searchInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
-
+// Search Item
 const SearchInput = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [searchInput, setSearchInput] = useState("");
