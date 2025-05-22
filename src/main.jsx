53c25e7b776a1d72f37698a52abad9e63b7a1367
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-22 20:14:49 +0600

    main  jsx 🚗

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-
+// Main js
 const router = createBrowserRouter([
   {
     path: "/",
